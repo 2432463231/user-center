147759d461b87b55b3b67a0e8ebf964bc420f2a6
86185 <521775>
2023-07-14 10:18:45 +0800

    测试数据库的功能，发现报错   userMapper没有注入

---
+++ src/test/java/com/example/user/SampleTest.java
@@ -0,0 +1,27 @@
+package com.example.user;
+
+import com.example.user.mapper.UserMapper;
+import com.example.user.model.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class SampleTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelect() {
+        System.out.println(("----- selectAll method test ------"));
+        List<User> userList = userMapper.selectList(null);
+        Assert.assertEquals(5, userList.size());
+        userList.forEach(System.out::println);
+    }
+
+}
